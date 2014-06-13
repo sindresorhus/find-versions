55f8c6761360fb72bafa9eb696ae2408438cc3be
Sindre Sorhus <sindresorhus@gmail.com>
2014-06-14 00:54:02 +0200

    init

---
+++ test.js
@@ -0,0 +1,13 @@
+'use strict';
+var assert = require('assert');
+var findVersions = require('./');
+
+it('should find versions in a string', function () {
+	assert.deepEqual(findVersions('unicorn 1.0.0 rainbow'), ['1.0.0']);
+	assert.deepEqual(findVersions('version 3.2.51(1)-release'), ['3.2.51']);
+	assert.deepEqual(findVersions('cp (GNU coreutils) 8.22'), []);
+	assert.deepEqual(findVersions('foo v2.0.0 rainbow 1.88'), ['2.0.0']);
+	assert.deepEqual(findVersions('foo v2.0.0 rainbow 1.88', {loose: true}), ['2.0.0', '1.88.0']);
+	assert.deepEqual(findVersions('cp (GNU coreutils) 8.22', {loose: true}), ['8.22.0']);
+	assert.deepEqual(findVersions('v1.0.0 foo 9.33 cp (GNU coreutils) 8.22 sad', {loose: true}), ['1.0.0', '9.33.0', '8.22.0']);
+});
