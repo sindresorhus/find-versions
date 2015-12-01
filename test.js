9fc0fea0d98291aa3181d3b4acd183d270edca5f
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-02 01:44:18 +0700

    add test for PHP version `7.0.0RC6`
    fixes #4

--- test.js
+++ test.js
@@ -13,5 +13,5 @@ test(t => {
 	t.same(fn('PHP 5.6.0beta1 (cli) (built: Aug 30 2014 13:52:33)\n Copyright (c) 1997-2014 The PHP Group\nZend Engine v2.6.0-dev, Copyright (c) 1998-2014 Zend Technologies\n    with Xdebug v2.2.4, Copyright (c) 2002-2014, by Derick Rethans', {loose: true}), ['5.6.0', '2.6.0-dev', '2.2.4']);
 	t.same(fn('Compass 0.12.7', {loose: true}), ['0.12.7']);
 	t.same(fn('1.14.2beta', {loose: true}), ['1.14.2']);
-	t.end();
+	t.same(fn('PHP 7.0.0RC6 (cli) (built: Oct 29 2015 13:46:05) ( NTS )\nCopyright (c) 1997-2015 The PHP Group\nZend Engine v3.0.0-dev, Copyright (c) 1998-2015 Zend Technologies', {loose: true}), ['7.0.0', '3.0.0-dev']);
 });
